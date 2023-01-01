c59f40182e97e5b29697d96ba3ac3c126bf47797
markus-ap <markusap@proton.me>
2023-01-02 00:20:02 +0100

    la til filmar

--- kode.js
+++ kode.js
@@ -4,6 +4,13 @@ let timeRemaining = targetTime - Date.now();  // Calculate the initial time rema
 // Update the countdown every 1000 milliseconds (1 second)
 const interval = setInterval(updateCountdown, 1000);
 
+window.onload = function(){
+    fyll_film();
+    setInterval(fyll_film, (1000*60))
+}
+
+const film = setInterval(fyll_film, (1000*10))
+
 function updateCountdown() {
   // Calculate the time remaining
   timeRemaining = targetTime - Date.now();
@@ -24,5 +31,58 @@ function updateCountdown() {
   // Display the time remaining
   var p = document.getElementById("tid");
   p.innerHTML = days + " dagar, " + hours + " timar, " + minutes + " minutt, og " + seconds + " sekund";
-  console.log(`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds remaining`);
+}
+
+function fyll_film(){
+  const samenlikning = document.getElementById("samenlikning");
+  const film = tilfeldig_film()
+  let antall = Math.round(timeRemaining / filmlengde_i_sekund(film["timar"], film["minutt"]));
+
+  samenlikning.innerHTML = "På den tida kan du sjå " + film["tittel"] + " " + antall + " gongar!";
+}
+
+function tilfeldig_film(){
+    const filmar = [
+        {
+            "tittel": "Avengers: Endgame",
+            "timar": 3,
+            "minutt": 2,
+            "sekund": 0
+        },
+        {
+            "tittel": "Love Actually",
+            "timar": 2,
+            "minutt": 9,
+            "sekund": 0
+        },
+        {
+            "tittel": "Skyfall",
+            "timar": 2,
+            "minutt": 23,
+            "sekund": 0
+        },
+        {
+            "tittel": "Spider-Man (2002)",
+            "timar": 2,
+            "minutt": 1,
+            "sekund": 0
+        },
+        {
+            "tittel": "Max Manus",
+            "timar": 1,
+            "minutt": 58,
+            "sekund": 0
+        },
+        {
+            "tittel": "Avatar",
+            "timar": 2,
+            "minutt": 42,
+            "sekund": 0
+        }
+    ]
+    return filmar[Math.floor(Math.random() * filmar.length)];
+}
+
+function filmlengde_i_sekund(timar, minutt){
+    return (timar * 60 + minutt) * 60 * 1000;
 }
